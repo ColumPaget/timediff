6150f01b766301ce664aa03d9369c3258c8e83c7
Colum Paget <colum.paget@axiomgb.com>
2025-02-12 19:25:15 +0000

    v2.1

--- main.c
+++ main.c
@@ -47,16 +47,19 @@ int ItemIncluded(const char *Line, double secs)
 
 
 
-#define ITEM_NORMAL 0
-#define ITEM_START  -1
-#define ITEM_END    -2
+#define ITEM_NORMAL   0
+#define ITEM_START   -1
+#define ITEM_END     -2
+#define ITEM_EXCLUDE -3
 
-int ProcessTimedItem(struct timeval *When, double Secs, const char *Line)
+int ProcessTimedItem(struct timeval *When, double Secs, const char *Line, char **Output)
 {
     const char *prefix="", *postfix="";
     char *Tempstr=NULL;
     int RetVal=ITEM_NORMAL;
 
+    //if a start is declared, and we have not encountered a start-line, or
+    //are between a start-line and an end-line, then ignore this line
     if (Started)
     {
         if ((Settings.WarnTime > -1) && (Secs > Settings.WarnTime))
@@ -71,8 +74,8 @@ int ProcessTimedItem(struct timeval *When, double Secs, const char *Line)
             postfix=Settings.NormPostfix;
         }
 
-        if (Settings.Flags & FLAG_NANOSECS) printf("%s%14.6f%s  %s\n", prefix, Secs, postfix, Line);
-        else printf("%s%10.3f%s  %s\n", prefix, Secs, postfix, Line);
+        if (Settings.Flags & FLAG_NANOSECS) *Output=FormatStr(*Output, "%s%14.6f%s  %s\n", prefix, Secs, postfix, Line);
+        else *Output=FormatStr(*Output, "%s%10.3f%s  %s\n", prefix, Secs, postfix, Line);
 
         StatsInsert("default", When, Secs);
 
@@ -92,18 +95,22 @@ int ProcessTimedItem(struct timeval *When, double Secs, const char *Line)
 
 
 
-double ProcessLine(struct timeval *Prev, struct timeval *Curr, const char *Line)
+double ProcessLine(struct timeval *Prev, struct timeval *Curr, const char *Line, char **Output)
 {
     double secs=0;
     int result;
 
     secs=CalcTimediff(Prev, Curr);
+
+    //if it's a 'Start' line, then we won't be calculating a difference between it
+    //and the previous line. Also, if a start is declared, then we will ignore lines
+    //before a start, or between a start and an end if an end is declared.
     if (InPatternList(Line, Settings.StartStrings, NULL))
     {
         //if we are in 'warn only' mode, where we only display items that have a time warning
         //then we want a 'start string' that matches the start of an interaction to be registered as the start time
         //but we don't want it displayed
-        if (! (Settings.Flags & FLAG_WARN_ONLY)) printf("%s%10s%s  %s\n", Settings.NormPrefix, " -- ", Settings.NormPostfix, Line);
+        if (! (Settings.Flags & FLAG_WARN_ONLY)) *Output=FormatStr(*Output, "%s%10s%s  %s\n", Settings.NormPrefix, " -- ", Settings.NormPostfix, Line);
         Started=TRUE;
         secs=ITEM_START;
     }
@@ -111,9 +118,10 @@ double ProcessLine(struct timeval *Prev, struct timeval *Curr, const char *Line)
     {
         if (ItemIncluded(Line, secs))
         {
-            result=ProcessTimedItem(Curr, secs, Line);
+            result=ProcessTimedItem(Curr, secs, Line, Output);
             if (result != ITEM_NORMAL) secs=(double) result;
         }
+        else secs=ITEM_EXCLUDE;
     }
 
     return(secs);
@@ -160,11 +168,52 @@ char *TimediffReadLine(char *RetStr, STREAM *S)
 }
 
 
+char *HandleStartEndBlock(char *RetStr, double secs, char *Line, struct timeval *PrevTime, struct timeval *CurrTime)
+{
+//if we aren't using start or end strings, then we will just output the current line
+//and PrevTime becomes the CurrTime. We don't use the OutputBuffer provided by RetStr
+    if ( (! StrValid(Settings.StartStrings)) && (! StrValid(Settings.EndStrings)) )
+    {
+        memcpy(PrevTime, CurrTime, sizeof(struct timeval));
+        printf("%s\n", Line);
+        return(CopyStr(RetStr, ""));
+    }
+
+    if (secs == ITEM_START)
+    {
+// on a start we always start a new output block,
+// and set PrevTime to CurrTime
+        memcpy(PrevTime, CurrTime, sizeof(struct timeval));
+        RetStr=CopyStr(RetStr, Line);
+    }
+    else if (secs == ITEM_END)
+    {
+//on an end we output the always buffered block of lines
+        RetStr=CatStr(RetStr, Line);
+        printf("%s\n", RetStr);
+        RetStr=CopyStr(RetStr, "");
+    }
+    else if (secs != ITEM_EXCLUDE)
+    {
+        RetStr=CatStr(RetStr, Line);
+
+//if we are measuring time from a start line, then we don't update PrevTime on every line
+        if  (! (Settings.Flags & FLAG_FROM_START)) memcpy(PrevTime, CurrTime, sizeof(struct timeval));
+        if (! StrValid(Settings.EndStrings))
+        {
+            printf("%s\n", RetStr);
+            RetStr=CopyStr(RetStr, "");
+        }
+    }
+
+    return(RetStr);
+}
+
 
 int main(int argc, const char *argv[])
 {
     STREAM *S;
-    char *Tempstr=NULL;
+    char *Tempstr=NULL, *InputLine=NULL, *Output=NULL;
     struct timeval Prev, Curr;
     double secs;
 
@@ -178,21 +227,17 @@ int main(int argc, const char *argv[])
     S=STREAMOpen(Settings.Input, "r");
     if (S)
     {
-        Tempstr=TimediffReadLine(Tempstr, S);
-        while (Tempstr)
+        InputLine=TimediffReadLine(InputLine, S);
+        while (InputLine)
         {
-            if (StrValid(Tempstr))
+            if (StrValid(InputLine))
             {
-                ParseDateTime(Tempstr, &Curr);
-                secs=ProcessLine(&Prev, &Curr, Tempstr);
-
-                if  (Settings.Flags & FLAG_FROM_START)
-                {
-                    if (secs == ITEM_START) memcpy(&Prev, &Curr, sizeof(struct timeval));
-                }
-                else memcpy(&Prev, &Curr, sizeof(struct timeval));
+                ParseDateTime(InputLine, &Curr);
+                secs=ProcessLine(&Prev, &Curr, InputLine, &Tempstr);
+                //actual printing of output happens in HandleStartEndBlock
+                Output=HandleStartEndBlock(Output, secs, Tempstr, &Prev, &Curr);
             }
-            Tempstr=TimediffReadLine(Tempstr, S);
+            InputLine=TimediffReadLine(InputLine, S);
         }
         //Display stats before closing stream,
         //because the stream might be stdin/stdout
@@ -202,6 +247,10 @@ int main(int argc, const char *argv[])
 
         STREAMClose(S);
     }
+
+    Destroy(InputLine);
+    Destroy(Tempstr);
+    Destroy(Output);
 
     return(0);
 }
